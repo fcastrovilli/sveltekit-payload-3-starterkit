6fa664d89f03a8f0a31c9a79392d9829f768c26a
Francesco Castrovilli <francesco.castrovilli95@gmail.com>
2024-07-05 23:03:07 +0200

    ui: small enhancement

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
 	export let data;
 	$: users = data.users;
+	import * as Card from '$lib/components/ui/card';
 </script>
 
-<div>
-	<h1>Create User</h1>
-	<form method="post">
-		<input type="email" name="email" placeholder="Email" />
-		<button type="submit">Submit</button>
-	</form>
+<div class="flex flex-col items-center justify-center gap-4 p-4">
+	<h1 class="text-3xl font-bold">Sveltekit + Payload CMS</h1>
+	<Card.Root>
+		<Card.Header>
+			<Card.Title>User Table {users ? '🎉' : '⛔'}</Card.Title>
+			<Card.Description>Doing a simple query to get all users</Card.Description>
+		</Card.Header>
+		<Card.Content>
+			{#if users}
+				<pre>{JSON.stringify(users.docs, null, 2)}</pre>
+				<div>
+					<h1 class="text-3xl font-semibold">Create User</h1>
+					<form method="post">
+						<input type="email" name="email" placeholder="Email" />
+						<button type="submit">Submit</button>
+					</form>
+				</div>
+			{:else}
+				<p>Can't connect to Payload CMS 😢</p>
+			{/if}
+		</Card.Content>
+	</Card.Root>
 </div>
-
-{#if users}
-	<h1>Users</h1>
-	<pre>{JSON.stringify(users?.docs, null, 2)}</pre>
-{/if}
